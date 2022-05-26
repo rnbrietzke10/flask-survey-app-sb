3bd804834425fc4d7803884fd277dfe1152508ca
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-05-26 12:33:33 -0500

    correct code to protect routes from user going to a question other than the next one and from trying to access a question that would be at a number past the last question in the survey

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+import re
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import *
@@ -14,24 +15,40 @@ responses = []
 def home_page():
     title = satisfaction_survey.title
     instruction = satisfaction_survey.instructions
-    return render_template('home.html', title = title, instruction = instruction )
+    quest_id = len(responses)
+
+    if len(satisfaction_survey.questions) == len(responses):
+        return redirect('/thank_you')
+    else:
+        return render_template('home.html', title = title, instruction = instruction,  quest_id = quest_id)
 
 @app.route('/questions/<question_id>')
 def question_page(question_id):
-    quest_id = int(question_id)
-    curr_question = list(satisfaction_survey.questions)[quest_id].question
-    answer_choices = list(satisfaction_survey.questions)[quest_id].choices
+    question_list = satisfaction_survey.questions
+    quest_id = len(responses)
+    if len(responses) == len(question_list):
+        return redirect('/thank_you')
+    else:
+        curr_question = question_list[quest_id].question
+        answer_choices = question_list[quest_id].choices
+        return render_template('question.html', curr_question = curr_question, quest_id = quest_id, answer_choices = answer_choices)
 
-    return render_template('question.html', curr_question = curr_question, quest_id = quest_id, answer_choices = answer_choices)
 
 @app.route('/answer', methods=["POST"])
 def answer_route():
+    if len(responses) == len(list(satisfaction_survey.questions)):
+        return redirect('/thank_you')
+    if(bool(list(request.form))):
+        list_split_key = list(request.form.keys())[0].split('-')
+        quest_id = int(list_split_key[1])
+    elif not bool(list(request.form)) or len(responses) != quest_id:
+        flash("Please select an answer.", 'error')
+        return redirect(f'/questions/{len(responses)}')
+
     list_split_key = list(request.form.keys())[0].split('-')
     responses.append(list_split_key[0])
-    quest_id = int(list_split_key[1]) + 1
-    if quest_id == 5:
-        return redirect('/thank_you')
-    return redirect(f'/questions/{int(quest_id)}')
+    quest_id = len(responses)
+    return redirect(f'/questions/{quest_id}')
 
 
 @app.route('/thank_you')
